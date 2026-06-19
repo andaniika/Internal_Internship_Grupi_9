821399e64cbf3d6c283706b985eea5ad661aacf7
grelakrasniqi <gk261@bgt.school>
2026-06-19 10:31:35 +0200

    Create lib main.dart

---
+++ day 3/task1/lib main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'quiz_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quiz App',
+      home: const QuizPage(),
+    );
+  }
+}
